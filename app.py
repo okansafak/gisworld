a3a1c0d3fbdb6ae8e7c711a57be286d2da617f17
okan <okn.safak@gmail.com>
2024-04-23 20:26:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
-import pydeck as pdk
 import pandas as pd
 import json
+import pydeck as pdk
+import matplotlib.pyplot as plt
 
 # Load the GeoJSON file containing station data
 with open("rayli_sistem_istasyon_poi_verisi.geojson", "r", encoding="utf-8") as f:
@@ -11,8 +12,24 @@ with open("rayli_sistem_istasyon_poi_verisi.geojson", "r", encoding="utf-8") as 
 features = station_data["features"]
 df = pd.json_normalize(features)
 
-# Display the GeoJSON data as a table
-st.write("GeoJSON Data", df)
+# Get value counts of HAT_TURU column
+hat_turu_counts = df["properties.HAT_TURU"].value_counts()
+
+# Calculate percentages
+percentages = (hat_turu_counts / hat_turu_counts.sum()) * 100
+
+# Create a DataFrame with counts and percentages
+data = pd.DataFrame({"Count": hat_turu_counts, "Percentage": percentages})
+
+# Plot the pie chart
+st.write("## Distribution of HAT_TURU")
+fig, ax = plt.subplots()
+ax.pie(data["Count"], labels=data.index, autopct="%1.1f%%", startangle=90)
+ax.axis("equal")  # Equal aspect ratio ensures that pie is drawn as a circle.
+st.pyplot(fig)
+
+# Display the data table
+st.write("## HAT_TURU Data", data)
 
 # Create a PyDeck map
 map = pdk.Deck(
@@ -31,5 +48,5 @@ map = pdk.Deck(
 )
 
 # Display the map using Streamlit
+st.write("## Map")
 st.pydeck_chart(map)
- 
