64bf75b0b2beb94a9afb33031b510c5c4e2fe648
okan <okn.safak@gmail.com>
2024-04-24 10:46:33 +0300

    aa

--- README.md
+++
@@ -1,5 +0,0 @@
-# Vehicle Incidents in England
-
-This is a streamlit app for visualizing vehicle incidents in England.  It also incorporates tableau.
-
-All data is stored and consumed in Snowflake.
--- app.py
+++ app.py
@@ -1,34 +1,30 @@
 import streamlit as st
+import pydeck as pdk
+import geopandas as gpd
 
+# İstanbul'un GeoJSON dosyasını yükle
+istanbul_geojson = 'istanbul.geojson'
+istanbul_data = gpd.read_file(istanbul_geojson)
 
-
-st.set_page_config(
-    page_title="Vehicle Incidents in England",
-    page_icon=":pickup_truck:",
-    initial_sidebar_state="expanded",
+# Pydeck için GeoJsonLayer oluştur
+layer = pdk.Layer(
+    'GeoJsonLayer',
+    data=istanbul_data,
+    get_fill_color=[200, 30, 0, 160],  # Rengi belirle (RGB formatında)
+    get_line_color=[255, 255, 255],  # Kenar çizgisinin rengi
+    lineWidthMinPixels=1,  # Kenar çizgisi kalınlığı
 )
 
-st.write("# VEHICLE INCIDENTS IN ENGLAND :pickup_truck:")
-
-st.sidebar.success("Select a demo above.")
-
-st.markdown(
-    """##### We are exploring Vehicle incidents accross england whilst utilising the capabilities of Streamlit, Snowflake, Carto and Tableau.
-    
-Click through the pages to view all towns in England based on real data, then see how this relates to Integrated Care Boards.
-
-Explore The Details of your chosen City and see how this is indexed into the H3 geospatial grid (Hexagons). Carto's Toolkit shared within Snowflake has been utilised for this. You may also see this within a radius of your chosen city.
-
-Finally, Explore How Vehicle incidents relate to fire service areas.
-
-All the raw data has been curated, engineered and processed with Snowflake. There are also 3 dashboards created using Tableau Cloud to visualise off live Snowflake data. Folium and Matplotlib has also been used for some visuals. Enjoy :smile:
-
-
-All code can be accessed here:
-
-https://github.com/beckyoconnor/Vehicle_Incidents_uk/
-
-
-"""
+# Harita yapısı oluştur
+view_state = pdk.ViewState(
+    latitude=41.0082,
+    longitude=28.9784,
+    zoom=10,
+    pitch=40.5,
 )
-                
+
+# Pydeck Haritasını oluştur
+r = pdk.Deck(layers=[layer], initial_view_state=view_state)
+
+# Streamlit ile göster
+st.pydeck_chart(r)
