2c8a5422fde178687d922e1777fc33f812347754
okan <okn.safak@gmail.com>
2024-04-24 10:56:58 +0300

    popup

--- app.py
+++ app.py
@@ -7,12 +7,23 @@ istanbul_geojson = 'istanbul.geojson'
 istanbul_data = gpd.read_file(istanbul_geojson)
 
 # Pydeck için GeoJsonLayer oluştur
-layer = pdk.Layer(
+geojson_layer = pdk.Layer(
     'GeoJsonLayer',
     data=istanbul_data,
     get_fill_color=[200, 30, 0, 160],  # Rengi belirle (RGB formatında)
     get_line_color=[255, 255, 255],  # Kenar çizgisinin rengi
     lineWidthMinPixels=1,  # Kenar çizgisi kalınlığı
+)
+
+# Popup verileri için ColumnLayer oluştur
+popup_layer = pdk.Layer(
+    "ColumnLayer",
+    data=istanbul_data,
+    get_position=["geometry.coordinates[0]", "geometry.coordinates[1]"],
+    get_elevation=1000,
+    get_fill_color=[255, 0, 0],
+    auto_highlight=True,
+    pickable=True
 )
 
 # Harita yapısı oluştur
@@ -24,7 +35,7 @@ view_state = pdk.ViewState(
 )
 
 # Pydeck Haritasını oluştur
-r = pdk.Deck(layers=[layer], initial_view_state=view_state)
+r = pdk.Deck(layers=[geojson_layer, popup_layer], initial_view_state=view_state, tooltip={"text": "Bu bir nokta"})
 
 # Streamlit ile göster
 st.pydeck_chart(r)
