b98ecdae98c153dee0e56ada0a5f9730806d040a
okan <okn.safak@gmail.com>
2024-04-25 17:49:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,27 +13,52 @@ def main():
     kvkk_approval = st.checkbox("KVKK metnini okudum ve kabul ediyorum.")
 
     if kvkk_approval:
-        # Ad Soyad
+        # Kişisel Bilgiler
+        st.header("Kişisel Bilgiler")
         full_name = st.text_input("Adınız Soyadınız")
-
-        # E-posta
         email = st.text_input("E-posta Adresiniz")
-
-        # Telefon Numarası
         phone = st.text_input("Telefon Numaranız")
 
+        # Eğitim Durumu
+        st.header("Eğitim Durumu")
+        graduate_status = st.radio("Mezun Durumu", ("Mezun", "Mezun Değil"))
+
+        if graduate_status == "Mezun":
+            university = st.text_input("Mezun Olduğunuz Üniversite")
+            department = st.text_input("Mezun Olduğunuz Bölüm")
+            grade = st.text_input("Mezuniyet Dereceniz")
+            employment_status = st.radio("Çalışma Durumu", ("Çalışıyor", "Çalışmıyor"))
+
+            if employment_status == "Çalışıyor":
+                company = st.text_input("Çalıştığınız Kurum")
+            else:
+                company = None
+        else:
+            university = st.text_input("Üniversite")
+            department = st.text_input("Bölüm")
+            grade = st.text_input("Sınıf")
+            company = None
+
+        # Programlama Bilgileri
+        st.header("Programlama Bilgileri")
+        programming_languages = st.multiselect("Kullandığınız Programlama Dilleri", ["Python", "R", "JavaScript", "C++", "Java", "Diğer"])
+
         # Mesaj
-        message = st.text_area("Mesajınız", height=150)
+        st.header("Eklemek İstediğiniz Mesaj")
+        message = st.text_area("Mesajınızı buraya yazabilirsiniz", height=150)
 
         # Başvuru Gönderme Butonu
         if st.button("Başvuru Gönder"):
             # Başvuru bilgilerini bir veri çerçevesine aktar
-            data = {'Ad Soyad': [full_name], 'E-posta': [email], 'Telefon': [phone], 'Mesaj': [message]}
+            data = {'Ad Soyad': [full_name], 'E-posta': [email], 'Telefon': [phone],
+                    'Üniversite': [university], 'Bölüm': [department], 'Sınıf': [grade],
+                    'Çalıştığı Kurum': [company], 'Programlama Dilleri': [", ".join(programming_languages)],
+                    'Mesaj': [message]}
             df = pd.DataFrame(data)
 
             # Veri çerçevesini Excel dosyasına yaz
             df.to_excel("basvurular.xlsx", index=False)
-            
+
             st.success("Başvurunuz başarıyla gönderildi! Teşekkür ederiz.")
     else:
         st.warning("Başvurunuz için KVKK metnini kabul etmelisiniz.")
