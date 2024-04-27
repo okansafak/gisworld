45546c61e330191fcfab2ef68ed9f129f64a6167
okan <okn.safak@gmail.com>
2024-04-28 00:46:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -60,11 +60,11 @@ if not filtrelenmis_okullar.empty:
     if secili_il != "Tümü":
         en_fazla_okul_turu = filtrelenmis_okullar["KURUM_TUR_ADI"].value_counts().idxmax()
         en_fazla_okul_sayisi = filtrelenmis_okullar["KURUM_TUR_ADI"].value_counts().max()
-        st.sidebar.metric(label="En Fazla Okul Türü", value=en_fazla_okul_sayisi, delta=0)
+        st.sidebar.metric(label="En Fazla Okul Türü", value=en_fazla_okul_sayisi, delta=0, delta_color='green')
         
         en_az_okul_turu = filtrelenmis_okullar["KURUM_TUR_ADI"].value_counts().idxmin()
         en_az_okul_sayisi = filtrelenmis_okullar["KURUM_TUR_ADI"].value_counts().min()
-        st.sidebar.metric(label="En Az Okul Türü", value=en_az_okul_sayisi, delta=0)
+        st.sidebar.metric(label="En Az Okul Türü", value=en_az_okul_sayisi, delta=0, delta_color='red')
 
     # Grafik: Okul türlerine göre dağılım
     st.subheader("Okul Türü Dağılımı")
